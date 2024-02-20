1282cc19aa0f8b1e1974f58e7a6de2a8034f3a53
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2024-02-20 17:29:05 -0500

    wip: parallelism
    wip: decide whether to do radix 2 bowers DFT parallelly or not
    
    Revert "wip: decide whether to do radix 2 bowers DFT parallelly or not"
    
    This reverts commit e13236944d3527fceaa26300ee40ee82059f3f86.
    
    remove timing code
    
    reduce parallelism in order to reduce user time without increasing wall clock time
    
    further reduce parallelism in order to drastically reduce user time while slightly increasing wall clock time
    
    parallelize fri query phase
    
    wip: further parallelize batch opening proofs
    
    wip: further parallelize batch opening proofs
    
    Revert "wip: further parallelize batch opening proofs"
    
    This reverts commit 8f620042474e1f7dcc64e26b5b5a8011b3d124f7.
    
    Revert "wip: further parallelize batch opening proofs"
    
    This reverts commit 424ea742164ad37374e6d91b62990482c85837ba.
    
    parallelize barycentric interpolation
    
    wip: run multiple barycentric interpolations in parallel
    
    wip: run multiple barycentric interpolations in parallel
    
    wip: run multiple barycentric interpolations in parallel
    
    run multiple barycentric interpolations in parallell
    
    remove println
    
    fix failing-to-compile test suite in valida
    
    Revert "fix failing-to-compile test suite in valida"
    
    This reverts commit ae450f9a44cce8346449c021b699bf855af75735.
    
    enable parallel feature flag by default so that Plonky3 will compile
    
    run cargo fmt
    
    clippy

--- dft/src/radix_2_bowers.rs
+++ dft/src/radix_2_bowers.rs
@@ -4,7 +4,6 @@ use p3_field::{Field, Powers, TwoAdicField};
 use p3_matrix::dense::{RowMajorMatrix, RowMajorMatrixViewMut};
 use p3_matrix::util::reverse_matrix_index_bits;
 use p3_matrix::Matrix;
-use p3_maybe_rayon::prelude::*;
 use p3_util::{log2_strict_usize, reverse_bits, reverse_slice_index_bits};
 
 use crate::butterflies::{
@@ -138,14 +137,14 @@ fn par_chunks_bowers<F: Field, Fun>(
 ) where
     Fun: Fn(&mut [F], &mut [F], F) + Sync,
 {
-    mat.par_row_chunks_mut(2 * half_block_size)
+    mat.row_chunks_mut(2 * half_block_size)
         .enumerate()
         .for_each(|(block, chunks)| {
             let (hi_chunks, lo_chunks) = chunks.split_at_mut(half_block_size * width);
             let twiddle = twiddles[block];
             hi_chunks
-                .par_chunks_exact_mut(width)
-                .zip(lo_chunks.par_chunks_exact_mut(width))
+                .chunks_exact_mut(width)
+                .zip(lo_chunks.chunks_exact_mut(width))
                 .for_each(|(hi_chunk, lo_chunk)| {
                     if block == 0 {
                         twiddle_free_butterfly_on_rows(hi_chunk, lo_chunk);
